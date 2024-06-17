4e1027b86a7c071327e7d9511e0d34633e4106ab
0212obzakaria <0212obzakaria@student.epfc.eu>
2024-06-17 21:37:26 +0200

    second

--- src/main/java/eu/epfc/java1888/fly/FlyController.java
+++ src/main/java/eu/epfc/java1888/fly/FlyController.java
@@ -3,6 +3,8 @@ package eu.epfc.java1888.fly;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -15,10 +17,22 @@ public class FlyController {
     }
 
     @GetMapping("/flies")
-    public String showFlies(Model model){
-    List<Fly> flies = repository.findAll();
-    model.addAttribute("list", flies);
-    return "flies-view";
+    public String showFlies(Model model) {
+        List<Fly> flies = repository.findAll();
+        model.addAttribute("list", flies);
+        return "flies-view";
+    }
 
+    @GetMapping("/fly-number")
+    public String showFlyNum(Model model){
+        Fly fly = new Fly();
+        model.addAttribute("fly", fly);
+        return "fly-view";
+    }
+
+    @PostMapping("/flies")
+    public String createGiftProcessor(@ModelAttribute("fly") Fly fly) {
+        repository.save(fly);
+        return "redirect:flies";
     }
 }
